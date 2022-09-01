a9d4ca8a80138d256d4408fe66b5a4e6927ccc07
evtcorreia <evtcorreia@gmail.com>
2022-09-01 13:14:18 -0300

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Granto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Granto.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UserController
+    {
+        private static List<User> users = new List<User>();
+
+        [HttpPost]
+        public void Adiciona([FromBody] User user)
+        {
+            users.Add(user);
+        }
+
+        [HttpGet]
+        public IEnumerable<User> getUsers()
+        {
+            return users;
+        }
+
+        
+
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Granto.Models
+{
+
+    
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string regiao { get; set; }
+
+    }
+}
